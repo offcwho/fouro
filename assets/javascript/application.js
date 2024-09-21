5d2af72c795b3d6fe9d27ddedb7b8134cafa5fa3
offc_who <nhnhnhbgbg@gmail.com>
2024-09-21 11:10:53 +0300

    add adaptive 520

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,1 +1,7 @@
 console.log('ok');
+let nav = document.getElementById("nav"),
+    button = document.getElementById("open")
+
+    button.addEventListener("click", () => {
+      nav.classList.toggle('header__nav--active')
+    })
